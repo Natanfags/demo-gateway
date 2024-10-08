0430d37580fdccb183cce9b5bcea4591571e30bc
natan.fagundes <natan.fags@gmail.com>
2024-10-08 10:08:01 -0300

    implementado primeiro filtro para testes

--- src/main/java/com/example/demogateway/DemoGatewayApplication.java
+++ src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -2,12 +2,40 @@ package com.example.demogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class DemoGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGatewayApplication.class, args);
 	}
+
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p
+						.host("*.cicuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
+
 
 }
